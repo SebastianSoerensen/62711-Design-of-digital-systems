6f117b94de04688d711dd30c0a02ebe6193c614f
SebastianSoerensen <Sebastiansoerensen97@outlook.dk>
2026-02-21 11:54:19 +0100

    .

--- project_1/project_1.srcs/sources_1/new/ALU.vhd
+++ project_1/project_1.srcs/sources_1/new/ALU.vhd
@@ -40,7 +40,18 @@ end ALU;
 
 architecture Behavioral of ALU is
 
+signal Y: std_logic_vector(7 downto 0);
+signal C_in: std_logic_vector(8 downto 0);
+
 begin
-
-
+C_in(0) <= '0'; -- No C_in for first Fulladder
+GEN_LOGIC: for i in 0 to 3 generate
+    Y(i) <= (not B(i) and J_Select(1)) or (B(i) and J_Select(0)); -- First 4 bits for J_Select(0) and J_Select(1)
+    Y(i+4) <= (not B(i+4) and J_Select(3)) or (B(i+4) and J_Select(2)); -- Last 4 bits for J_Select(2) and J_Select(3) indexed from 
+end generate;
+GEN_FA: for i in 0 to 7 generate
+    FA : entity work.Full_adder port map(A => A(i), B => Y(i), C_in => C_in(i), S => J(i), C_out => C_in(i+1));
+end generate;
+C <= C_in(8); -- Carry out
+V <= C_in(7) xor C_in(8); -- Overflow detection
 end Behavioral;
