1bb993d783e6fe896417689fdc65224c87cc42cd
Mystfallrpg <oliverhsally@gmail.com>
2026-02-23 06:50:03 +0100

    .

--- project_1/project_1.srcs/sources_1/new/ALU.vhd
+++ project_1/project_1.srcs/sources_1/new/ALU.vhd
@@ -1,95 +1,40 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 15.02.2026 16:11:56
--- Design Name: 
--- Module Name: ALU - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity ALU is
-    Port ( A,B : in STD_LOGIC_VECTOR (7 downto 0);
-           J_Select : in STD_LOGIC_VECTOR (3 downto 0);
+    Port ( A,B : in  STD_LOGIC_VECTOR (7 downto 0);
+           J_Select : in  STD_LOGIC_VECTOR (3 downto 0);
            V,C : out STD_LOGIC;
            J : out STD_LOGIC_VECTOR (7 downto 0));
 end ALU;
 
 architecture Behavioral of ALU is
 
--- Decoder signals
-signal Decoder_EN : std_logic;
-signal EN : std_logic_vector(3 downto 0);
-signal Function_EN : std_logic_vector(15 downto 0);
-
--- Arithmetic signals
-signal arith_C, arith_V, Plus_one: std_logic; -- result, carry, overflow
-signal S_arith : std_logic_vector(1 downto 0); -- select
-signal arith_res : std_logic_vector(7 downto 0);
-
--- Logic results signals
-signal or_res, and_res, xor_res, not_res : std_logic_vector(7 downto 0);
+signal arith_C, arith_V, Plus_One : std_logic;
+signal S_arith : std_logic_vector(1 downto 0);
+signal Function_EN : std_logic_vector(3 downto 0);
+signal arith_res: std_logic_vector(7 downto 0);
 
 begin
+Decoder_FS: entity work.Decoder_2to4_Enable port map(
+	EN => J_Select(3), S => J_Select(1 downto 0), D => Function_EN);
 
-Decoder_EN <= '1';
-
--- Decoder for 2 LSB (J_Select(1 downto 0)
-Decoder_FS: entity work.Decoder_2to4_Enable port map(EN => Decoder_EN, S => J_Select(1 downto 0), D => EN );
-
--- Decoder for 2 MSB (J_select(3 downto 2))
-GEN_DEC : for i in 0 to 3 generate
-    DEC: entity work.Decoder_2to4_Enable port map(EN => EN(i) , S => J_Select(3 downto 2), D => Function_EN(4*i+3 downto 4*i));
-end generate;
-
--- +1 Carry_in, Select Arithmetic
 Plus_One <= J_Select(0);
 S_arith <= J_Select(2 downto 1);
 
-ARITH: entity work.Arith_Circ port map(C_in => Plus_One, S => S_arith, A => A, B => B, G => arith_res, C_out => arith_C, Overflow => arith_V);
-        
--- Logic operations (does all at the same time but it takes almost no hardware power)
-GEN_LOGIC: for i in 0 to 7 generate
-    or_res(i) <= A(i) or B(i);
-    and_res(i) <= A(i) and B(i);
-    xor_res(i) <= A(i) xor B(i);
-    not_res(i) <= not A(i);
-end generate;
+ARITH: entity work.Arith_Circ port map(
+	C_in => Plus_One, S => S_arith, A => A, B => B, G => arith_res, C_out => arith_C, Overflow => arith_V);
 
--- Select logic or aritmetic operations
 GEN_OUT: for i in 0 to 7 generate
     J(i) <= arith_res(i) when J_Select(3) = '0'
-    else (or_res(i) and Function_EN(8))
-    or (and_res(i) and Function_EN(9))
-    or (xor_res(i) and Function_EN(10))
-    or (not_res(i) and Function_EN(11));
+       else ((A(i) or  B(i)) and Function_EN(1))
+       or ((A(i) and B(i)) and Function_EN(0))
+       or ((A(i) xor B(i)) and Function_EN(2))
+       or ((not A(i)) and Function_EN(3));
 end generate;
 
--- Overflow and Carry_out are only relevant for the fulladders in Arith_Circ
-V <= arith_V when J_Select(3) = '0' else '0'; 
+-- Flags only relevant for arithmetic because of full adder
+V <= arith_V when J_Select(3) = '0' else '0';	
 C <= arith_C when J_Select(3) = '0' else '0';
 
 end Behavioral;
